df2abce94954bec9bcd6c843dc35fef8408fbf0d
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-02-06 18:28:21 -0300

    Update ReviewBox.jsx

--- src/components/ReviewBox/ReviewBox.jsx
+++ src/components/ReviewBox/ReviewBox.jsx
@@ -20,8 +20,8 @@ export default function ReviewBox({ title, content, createdAt, rating, userName 
 
 const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Sempre com 2 dígitos
-    const day = String(date.getDate()).padStart(2, '0'); // Sempre com 2 dígitos
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     const year = date.getFullYear();
     
     return `${month}/${day}/${year}`;
