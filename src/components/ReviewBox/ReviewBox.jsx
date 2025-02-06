2cb1391c1d07bdca0d0fb79b7ee2a94771cc74f7
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-02-06 18:27:07 -0300

    Review Box styles

--- src/components/ReviewBox/ReviewBox.jsx
+++ src/components/ReviewBox/ReviewBox.jsx
@@ -1,14 +1,28 @@
+import { Rating } from 'react-simple-star-rating';
 import './ReviewBox.scss';
 
 // eslint-disable-next-line react/prop-types
 export default function ReviewBox({ title, content, createdAt, rating, userName }) {
     return (
         <div className="review-box">
-            <p>{title}</p>
-            <p>{content}</p>
-            <p>{createdAt}</p>
-            <p>{rating}</p>
-            <p>{userName}</p>
+            <Rating size={24} readonly={true} initialValue={rating} />
+            <div className="review-box__user-info">
+                <p>{userName},</p>
+                <p>{formatDate(createdAt)}</p>
+            </div>
+            <div className='review-box__review-info'>
+                <p className='review-box__title'>{title}</p>
+                <p className='review-box__content'>{content}</p>
+            </div>
         </div>
     );
 }
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Sempre com 2 dígitos
+    const day = String(date.getDate()).padStart(2, '0'); // Sempre com 2 dígitos
+    const year = date.getFullYear();
+    
+    return `${month}/${day}/${year}`;
+};
